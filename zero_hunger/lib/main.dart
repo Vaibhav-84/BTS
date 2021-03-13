363250d6fb3f4df6e70c49c59546b496f5c4b3d0
Vaibhav Bhapkar <vaibhavbhapkar62@gmail.com>
2021-03-13 18:49:44 +0530

    Worked on upload and feed

---
+++ zero_hunger/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:zero_hunger/helper/authenticate.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:zero_hunger/helper/helperfunctions.dart';
+import 'package:zero_hunger/views/chatRoomsScreen.dart';
+import 'package:zero_hunger/views/feed.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool userIsLoggedIn = false;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await HelperFunctions.getUserLoggedInSharedPreference().then((value) {
+      setState(() {
+        userIsLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zero Hunger',
+      debugShowCheckedModeBanner: false,
+      home: userIsLoggedIn ? DisplayFeed() : Authenticate(),
+    );
+  }
+}
